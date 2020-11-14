589a65bc78185acc35d7c359494fdc348b5fb32b
ShouldveBeenMe <ShouldveBeenMe@users.noreply.github.com>
2020-11-14 23:07:02 +0200

    Start pointCloud method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.sparse as sparse
+import pyvista as pv
 from read_off import read_off 
+
 
 class Mesh:
 
@@ -10,9 +12,32 @@ class Mesh:
     def vertex_face_adjacency(self):
         v_idx = 1+np.arange(len(self.v))
         adj = [np.isin(v_idx, f) for f in self.f]
-        return sparse.csr_matrix(adj).transpose()
+        self.vf_a = sparse.lil_matrix(adj).transpose()
+        return sparse.lil_matrix(adj).transpose()
 
     def vertex_vertex_adjacency(self):
-        v_idx = 1+np.arange(len(self.v))
-        adj = [np.isin(v_idx, f) for f in self.f]
-        return sparse.csr_matrix(adj).transpose()
+        vf_adj = self.vertex_face_adjacency()
+        vf_adj = vf_adj.astype(int)
+        common_face_num = np.dot(vf_adj, vf_adj.transpose())
+        common_face_bool = common_face_num >= 2 # Triangle sides - 1
+        common_face_bool.setdiag(False)
+        self.vv_a = common_face_bool
+        return self.vv_a
+
+    def vertex_degree(self):
+        vv_a = self.vertex_vertex_adjacency()
+        res = vv_a.sum(axis=0)
+        return res
+
+    def render_wireframe(self):
+        plotter = pv.Plotter()
+        plotter.add_mesh(self.v, style='wireframe')
+        plotter.show()
+        return plotter
+
+    def render_pointcloud(self, scalar_func):
+        pointcloud = pv.PolyData(self.v)
+        plotter = pv.Plotter()
+        plotter.add_mesh(pointcloud, render_points_as_spheres=True, cmap=scalar_func)
+        plotter.show()
+        return plotter
--- tests.py
+++ tests.py
@@ -1,13 +1,19 @@
 from read_off import read_off 
 from write_off import write_off 
 from main import Mesh
+import numpy as np
 
 (v, f) = read_off('off_files/example_off_files/sphere_s0.off')
 write_off('output.off', v, f)
 
 sphere_mesh = Mesh('off_files/example_off_files/sphere_s0.off')
-adj_mat = sphere_mesh.vertex_face_adjacency()
+vf_adj = sphere_mesh.vertex_face_adjacency()
+vv_adj = sphere_mesh.vertex_vertex_adjacency()
+v_deg = sphere_mesh.vertex_degree()
+# plotter = sphere_mesh.render_wireframe()
+colormap = np.linspace(0,256,162)
+plotter = sphere_mesh.render_pointcloud(colormap)
+print('end')
 
 
-print
 
