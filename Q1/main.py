80cbe4a749b4a8ccf37a46d89e16d870f9d45c73
ShouldveBeenMe <ShouldveBeenMe@users.noreply.github.com>
2020-11-27 13:04:39 +0200

    Continue Q1

--- Q1/main.py
+++ Q1/main.py
@@ -9,6 +9,7 @@ class Mesh:
 
     def __init__(self, off_path):
         self.v, self.f = read_off(off_path)
+        self.f = self.f[:,1:]
         self.f_v_map = np.array([[self.v[v_idx] for v_idx in face] for face in self.f])
 
     
@@ -105,14 +106,13 @@ class Mesh:
 
         for i in range (self.v.shape[0]):
             f_inds = sparse.find(vf_a[i,:])[1]
-            v_inds = self.f[f_inds,  1:]
+            v_inds = self.f[f_inds]
             v = self.v[i]
             v_coords = self.v[v_inds[v_inds != i+1].reshape((-1,2)),:]
             my_v = self.v[i].reshape((1,1,v.shape[0]))
             f_edges = v_coords - my_v
             sum_angles = sum([compute_angle(edges[0], edges[1]) for edges in f_edges])
             self.gauss_curv[i] = (2*np.pi - sum_angles)/self.bc_v_areas[i]
-            print('yalla')
         
         print('achieved')
             
--- Q1/tests.py
+++ Q1/tests.py
@@ -8,10 +8,10 @@ import numpy as np
 write_off('output.off', v, f)
 
 sphere_mesh = Mesh('off_files/example_off_files/sphere_s0.off')
-# vf_adj = sphere_mesh.vertex_face_adjacency()
-# vv_adj = sphere_mesh.vertex_vertex_adjacency()
-# v_deg = sphere_mesh.vertex_degree()
-# plotter = sphere_mesh.render_wireframe()
+vf_adj = sphere_mesh.vertex_face_adjacency()
+vv_adj = sphere_mesh.vertex_vertex_adjacency()
+v_deg = sphere_mesh.vertex_degree()
+plotter = sphere_mesh.render_wireframe()
 # boring_cmap = plt.cm.get_cmap("viridis", 162)
 
 # plotter = sphere_mesh.render_pointcloud(boring_cmap)
